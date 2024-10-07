9f6499c960db983701d222af5655e4e925b5887c
ricardo <carlos.manso@mobentis.com>
2024-10-07 13:07:46 +0200

    comienzo de arreglo a fechas

--- src/app/components/grafica-pedidos-dia/grafica-pedidos-dia.component.ts
+++ src/app/components/grafica-pedidos-dia/grafica-pedidos-dia.component.ts
@@ -3,7 +3,6 @@ import * as echarts from 'echarts/core';
 import { GridComponent, GridComponentOption } from 'echarts/components';
 import { BarChart, BarSeriesOption } from 'echarts/charts';
 import { CanvasRenderer } from 'echarts/renderers';
-import { left } from '@popperjs/core';
 
 echarts.use([GridComponent, BarChart, CanvasRenderer]);
 
